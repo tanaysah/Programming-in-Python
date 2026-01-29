dcbda27a04e3dd6be6f403cb16d75707b6bf9a0c
Tanay Sah <sah_cse@hotmail.com>
2026-01-29 13:04:54 +0530

    Add files via upload

---
+++ day 1.py
@@ -0,0 +1,27 @@
+#tanay sah
+''' tanay sah '''
+' tanay sah '
+''''''''''''''''' tanay sah'''
+print("hello ji")
+a=5
+b=6.2
+c=7
+d=8.25
+
+sum=a+b+c+d
+sub=a-b-c-d
+mul=a*b*c
+div=a//b//c
+
+print(sum)
+print(sub)
+print(mul)
+print(div)
+
+
+name=input("what is your name? \n")
+age=input ("what is your age? \n")
+gender= input ("what is your gender? \n")
+
+print("name \n age\ngender\n")
+

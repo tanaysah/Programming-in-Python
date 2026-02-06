e6b4988a981af5bac932025c2a0086112a71fb48
Tanay Sah <sah_cse@hotmail.com>
2026-02-06 12:58:23 +0530

    Add files via upload

---
+++ py19.19.py
@@ -0,0 +1,11 @@
+# Using single quotes
+str1 = 'Hello Python'
+print(str1)
+
+str2 = "Hello Python"
+print(str2)
+
+str3 = '''Triple quotes are generally used for
+represent the multiline or
+docstring'''
+print(str3)
---
+++ py20.20.py
@@ -0,0 +1,9 @@
+str = "HELLO"
+
+print(str[0])
+print(str[1])
+print(str[2])
+print(str[3])
+print(str[4])
+
+print(str[2])

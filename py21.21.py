5b05dc9c016945707ad73308af80d63b96089817
Tanay Sah <sah_cse@hotmail.com>
2026-02-06 12:58:40 +0530

    Add files via upload

---
+++ py21.21.py
@@ -0,0 +1,10 @@
+str = "Hello"
+str1 = " world"
+
+print(str * 3)              # HelloHelloHello
+print(str + str1)           # Hello world
+print(str[4])               # o
+print(str[2:4])             # ll
+print('w' in str)           # False
+print('wo' not in str1)     # False
+print("The string str : %s" % (str))

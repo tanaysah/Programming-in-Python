ea4a12547f9c5eade68a2b2a724689f4db512e6c
Tanay Sah <sah_cse@hotmail.com>
2026-02-06 12:57:33 +0530

    Add files via upload

---
+++ py07.7.py
@@ -0,0 +1,11 @@
+# Exit the loop when i is 3
+i = 1
+
+while i < 6:
+    if i == 3:
+        print(i)
+        break
+    print(i)
+    i += 1
+
+print("hh")
---
+++ py10.10.py
@@ -0,0 +1,4 @@
+n = int(input("Enter the number: "))
+
+for i in range(2, n, 2):
+    print(i)
